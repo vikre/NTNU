b64e003b8f1bcf6673ec1e0dc1883604123caa9c
vikre <vikre.90@gmail.com>
2014-01-13 10:35:52 +0100

    sprite moving in the right directions and facing the right way

--- TDT4240/Game/src/com/example/helicopter/Sprite.java
+++ TDT4240/Game/src/com/example/helicopter/Sprite.java
@@ -31,20 +31,26 @@ public class Sprite {
 	public void onDraw(Canvas canvas){
 		update();
 		int mapx = currentFrame*width;
-		int mapy = 1*height;
+		int mapy = animateDirection()*height;
 		Rect startpkt = new Rect(mapx,mapy,mapx+width,mapy+height);
 		Rect endpkt = new Rect(x,y,x+width,y+height);
-		canvas.drawBitmap(bitmap, mapx, mapy, null);
+		canvas.drawBitmap(bitmap, startpkt, endpkt, null);
 	}
-	
+	private int animateDirection(){
+		int[] directions = {3, 1, 0, 2};
+		double formulaX = (Math.atan2(speedX, speedY) / (Math.PI / 2) + 2);
+        int dir = (int) Math.round(formulaX) % ROWS;
+        return directions[dir];
+	}
 	private void update(){
-		if(x > gameView.getWidth() - width-speedX){
-			speedX = -5;
-		}
-		if(x + speedX < 0){
-			speedX = 5;
+		if(x > gameView.getWidth() - width-speedX|| x + speedX < 0){
+			speedX = -speedX;
 		}
 		x = x + speedX;
+		if(y > gameView.getHeight() - height-speedY || y+speedY < 0){
+			speedY = -speedY;
+		}
+		y = y + speedY;
 		currentFrame = ++currentFrame %COLUMNS;//get the next frame
 	}
 }
